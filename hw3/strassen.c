2d81d46214fa251e456271c37609796391867cf8
Ubuntu <ubuntu@ip-172-31-8-226.ec2.internal>
2015-09-23 06:57:38 +0000

    pthread version

--- hw3/strassen.c
+++ hw3/strassen.c
@@ -3,7 +3,7 @@
  * Department of Computer Science
  * CS1645: Introduction to HPC Systems
  * Instructor Bryan Mills, PhD
- * Student: 
+ * Student:
  * Implement Pthreads version of Strassen algorithm for matrix multiplication.
  */
 
@@ -20,9 +20,172 @@
 int **A;
 int **B;
 int **C;
+// addition matrixes
+int **M[7];
+int **C_sub[4];
 // Reference matrix, call simpleMM to populate.
 int **R;
 
+// Calculation function
+int **allocMatrix(int size);
+void mat_pad(int ***mat, int N, int new_N) {
+    int **new_mat = allocMatrix(new_N);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            new_mat[i][j] = (*mat)[i][j];
+        }
+    }
+    int **temp;
+    temp = *mat;
+    *mat = new_mat;
+    free(temp);
+}
+static inline void mat_add(int **dest, int **source1, int loc1, int **source2, int loc2, int N) {
+    int row_1 = loc1 / N;
+    int col_1 = loc1 % N;
+    int row_2 = loc2 / N;
+    int col_2 = loc2 % N;
+    for (int i = 0; i < N / 2; ++i) {
+        for (int j = 0; j < N / 2; ++j) {
+            dest[i][j] = source1[row_1 + i][col_1 + j] + source2[row_2 + i][col_2 + j];
+        }
+    }
+}
+static inline void mat_sub(int **dest, int **source1, int loc1, int **source2, int loc2, int N) {
+    int row_1 = loc1 / N;
+    int col_1 = loc1 % N;
+    int row_2 = loc2 / N;
+    int col_2 = loc2 % N;
+    for (int i = 0; i < N / 2; ++i) {
+        for (int j = 0; j < N / 2; ++j) {
+            dest[i][j] = source1[row_1 + i][col_1 + j] - source2[row_2 + i][col_2 + j];
+        }
+    }
+
+}
+static inline void mat_mul(int **dest, int **source1, int loc1, int **source2, int loc2, int N){
+    int row_1 = loc1 / N;
+    int col_1 = loc1 % N;
+    int row_2 = loc2 / N;
+    int col_2 = loc2 % N;
+    for (int i=0; i<N / 2; i++) {
+        for (int j=0; j<N / 2; j++) {
+            for (int k=0; k<N / 2; k++) {
+                dest[i][j] += source1[row_1 + i][col_1 + k] * source2[row_2 + k][col_2 + j];
+            }
+        }
+    }
+
+}
+
+//M function
+void* M_func1(void* N);
+void* M_func2(void* N);
+void* M_func3(void* N);
+void* M_func4(void* N);
+void* M_func5(void* N);
+void* M_func6(void* N);
+void* M_func7(void* N);
+void* (*M_func[7])(void*) = {M_func1, M_func2, M_func3, M_func4, M_func5, M_func6, M_func7};
+void C_func1(int N);
+void C_func2(int N);
+void C_func3(int N);
+void C_func4(int N);
+void (*C_func[4])(int) = {C_func1, C_func2, C_func3, C_func4};
+void combine_C(int N);
+
+void* M_func1(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    int **temp_M_2 = allocMatrix(N);
+    mat_add(temp_M_1, A, 0, A, N * N / 2 + N / 2, N);
+    mat_add(temp_M_2, B, 0, B, N * N / 2 + N / 2, N);
+    mat_mul(M[0], temp_M_1, 0, temp_M_2, 0, N);
+    free(temp_M_1);
+    free(temp_M_2);
+}
+void* M_func2(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    mat_add(temp_M_1, A, N * N /2, A, N * N / 2 + N / 2, N);
+    mat_mul(M[1], temp_M_1, 0, B, 0, N);
+    free(temp_M_1);
+}
+void* M_func3(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    mat_sub(temp_M_1, B, N / 2, B, N * N / 2 + N / 2, N);
+    mat_mul(M[2], A, 0, temp_M_1, 0, N);
+    free(temp_M_1);
+}
+void* M_func4(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    mat_sub(temp_M_1, B, N * N / 2, B, 0, N);
+    mat_mul(M[3], A, N * N / 2 + N / 2, temp_M_1, 0, N);
+    free(temp_M_1);
+}
+void* M_func5(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    mat_add(temp_M_1, A, 0, A, N / 2, N);
+    mat_mul(M[4], temp_M_1, 0, B, N * N / 2 + N / 2, N);
+    free(temp_M_1);
+}
+void* M_func6(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    int **temp_M_2 = allocMatrix(N);
+    mat_sub(temp_M_1, A, N * N / 2, A, 0, N);
+    mat_add(temp_M_2, B, 0, B, N / 2, N);
+    mat_mul(M[5], temp_M_1, 0, temp_M_2, 0, N);
+    free(temp_M_1);
+    free(temp_M_2);
+}
+void* M_func7(void* rank) {
+    int* rank_ptr = (int *) rank;
+    int N = *rank_ptr;
+    int **temp_M_1 = allocMatrix(N);
+    int **temp_M_2 = allocMatrix(N);
+    mat_sub(temp_M_1, A, N / 2, A, N * N / 2 + N / 2, N);
+    mat_add(temp_M_2, B, N * N / 2, B, N * N / 2 + N / 2, N);
+    mat_mul(M[6], temp_M_1, 0, temp_M_2, 0, N);
+    free(temp_M_1);
+    free(temp_M_2);
+}
+void C_func1(int N) {
+    mat_add(C_sub[0], M[0], 0, M[3], 0, N);
+    mat_add(C_sub[0], C_sub[0], 0, M[6], 0, N);
+    mat_sub(C_sub[0], C_sub[0], 0, M[4], 0, N);
+}
+void C_func2(int N) {
+    mat_add(C_sub[1], M[2], 0, M[4], 0, N);
+}
+void C_func3(int N) {
+    mat_add(C_sub[2], M[1], 0, M[3], 0, N);
+}
+void C_func4(int N) {
+    mat_add(C_sub[3], M[0], 0, M[2], 0, N);
+    mat_add(C_sub[3], C_sub[3], 0, M[5], 0, N);
+    mat_sub(C_sub[3], C_sub[3], 0, M[1], 0, N);
+}
+void combine_C(int N) {
+    for (int i = 0; i < 4; ++i) {
+        int row = (i / 2) * N / 2;
+        int col = (i % 2) * N / 2;
+        for (int j = 0; j < N / 2; ++j) {
+            for (int k = 0; k < N / 2; ++k) {
+                C[row+j][col+k] = C_sub[i][j][k];
+            }
+        }
+    }
+}
 // Stupid simple Matrix Multiplication, meant as example.
 void simpleMM(int N) {
   for (int i=0; i<N; i++) {
@@ -38,7 +201,41 @@ void simpleMM(int N) {
 // of the sub-matrixes you will need to calculate but you can create your
 // threads in this fucntion.
 void strassenMM(int N) {
-  
+    int new_N = 1;
+    while (new_N < N) {
+        new_N *= 2;
+    }
+    // pad matrix
+
+    timerStart();
+    if (N != new_N) {
+        mat_pad(&A, N, new_N);
+        mat_pad(&B, N, new_N);
+        mat_pad(&C, N, new_N);
+        for (int i = 0; i < 7; ++i) {
+            mat_pad(&M[i], N, new_N);
+        }
+        for (int i = 0; i <4; ++i) {
+            mat_pad(&C_sub[i], N, new_N);
+        }
+
+    }
+    printf("matrix pad took %ld ms\n", timerStop());
+    N = new_N;
+    pthread_t ids[7];
+    timerStart();
+    for (int i = 0; i < 7; ++i) {
+        pthread_create(&ids[i], NULL, *M_func[i], &N);
+    }
+    for (int i=0; i < 7; i++) {
+        pthread_join(ids[i], NULL);
+    }
+    printf("seven M funcs took %ld ms\n", timerStop());
+    C_func1(N);
+    C_func2(N);
+    C_func3(N);
+    C_func4(N);
+    combine_C(N);
 }
 
 // Allocate square matrix.
@@ -60,6 +257,12 @@ int **allocMatrix(int size) {
 // here to initialize any matrixes that you need.
 void initMatrixes(int N) {
   A = allocMatrix(N); B = allocMatrix(N); C = allocMatrix(N); R = allocMatrix(N);
+  for (int i = 0; i < 7; ++i) {
+       M[i] = allocMatrix(N);
+  }
+  for (int i = 0; i < 4; ++i) {
+      C_sub[i] = allocMatrix(N);
+  }
 }
 
 // Free up matrixes.
@@ -118,7 +321,7 @@ int main(int argc, char* argv[]) {
   }
 
   // stopping timer
-  
+
   cleanup();
   return 0;
 }
